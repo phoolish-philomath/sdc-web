3fd760913bd56d8e0988b64087f6ce079fa87542
Sana Farooqui <sana@vubblepop.com>
2021-08-20 09:33:53 -0400

    Updates

---
+++ precache-manifest.38f217567bba645e0258428b4cb57104.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fab5479f67baf5cf3f0fb4ab5de7d491",
+    "url": "/sdc-web/index.html"
+  },
+  {
+    "revision": "6aba885687d832a46e72",
+    "url": "/sdc-web/static/js/2.71de7553.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/sdc-web/static/js/2.71de7553.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90b1744e695cd7cc495f",
+    "url": "/sdc-web/static/js/main.da049d76.chunk.js"
+  },
+  {
+    "revision": "952d9b198878c968b9b4",
+    "url": "/sdc-web/static/js/runtime-main.ebb03cda.js"
+  },
+  {
+    "revision": "59c4667d1cc392c19661b8923c8eda66",
+    "url": "/sdc-web/static/media/SocialDevelopmentCentre_Logo_WhiteOnBlack_1.59c4667d.png"
+  }
+]);
