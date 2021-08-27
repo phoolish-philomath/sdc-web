990206e512e8b82dc2cd7cba164c0291474ea062
Sana Farooqui <sana@vubblepop.com>
2021-08-27 10:31:53 -0400

    Removing homepage change that was used for testing

--- src/App/Components/Home.jsx
+++ src/App/Components/Home.jsx
@@ -104,7 +104,7 @@ export default class Home extends React.Component {
     return (
       <Banner>
         <HeaderTitle>
-          Welcome Kira and Todd to the phoolish deployment of the{' '}
+          Welcome to the{' '}
           <BoldedWord>Social Development Centre Waterloo Region.</BoldedWord>
         </HeaderTitle>
         <HeaderDescription>
